6824458fdb1c06462d4b3214ea21e531b5e8c6f3
lunfman <lunf@Olegs-MacBook-Air.local>
2021-12-05 19:56:30 +0200

    added rename_command and open_command methods. Also did unittest for each method. Removed some comments

--- tests/test_terminal.py
+++ tests/test_terminal.py
@@ -21,5 +21,39 @@ class TestTerminal(unittest.TestCase):
         self.assertEqual(self.terminal.get_command('OPEN new tab').cur_commands, [])
 
 
+    def test_open_command(self):
+        self.terminal.input = 'Open tasks'
+        self.assertEqual(self.terminal.open_command(), 'tasks')
+        self.terminal.input = 'Open new category'
+        self.assertEqual(self.terminal.open_command(), 'new category')
+        self.terminal.input = 'Open flowers'
+        self.assertEqual(self.terminal.open_command(), 'flowers')
+        self.terminal.input = 'open tasks'
+        self.assertEqual(self.terminal.open_command(), None)
+        self.terminal.input = 'command Open'
+        self.assertEqual(self.terminal.open_command(), None)
+
+    
+    def test_rename_method(self):
+        # simple input
+        self.terminal.input = 'Rename tasks To task1'
+        self.assertEqual(self.terminal.rename_command(), ['tasks', 'task1'])
+        # long cat name
+        self.terminal.input = 'Rename task task task To task1 task1 task1'
+        self.assertEqual(self.terminal.rename_command(), ['task task task', 'task1 task1 task1'])
+        # wrong format
+        self.terminal.input = 'Rename tasks to task1'
+        self.assertEqual(self.terminal.rename_command(), None)
+        self.terminal.input = 'rename tasks To task1'
+        self.assertEqual(self.terminal.rename_command(), None)
+        self.terminal.input = 'rename tasks'
+        self.assertEqual(self.terminal.rename_command(), None)
+        self.terminal.input = 'rename tasks to'
+        self.assertEqual(self.terminal.rename_command(), None)
+        self.terminal.input = 'rename to'
+        self.assertEqual(self.terminal.rename_command(), None)
+        self.terminal.input = 'rename tasks tasks1'
+        self.assertEqual(self.terminal.rename_command(), None)
+
 if __name__ == '__main__':
     unittest.main()
--- todoapp/terminal.py
+++ todoapp/terminal.py
@@ -3,81 +3,112 @@ from .date_manage import manageDeadlines
 from .models import db, Tasks
 
 class Terminal:
+    
+    '''
+    Terminal class allows to manage users inputs from the app
+    '''
 
-
-    # how it is going to work? Users type something in the terminal. Flask receive message and send it 
-    # to Terminal class
+    # how it is going to work? Users type something in the terminal.
+    #  Flask receive message and send it to Terminal class
     # 1) Get commands from the input to validate what user want to do
     # 2) After do what user wants
     # 3) Return whatever needed 
 
 
     def __init__(self):
+        
         # commands which using our app for interacting with the system/app
         # strict names
-        self.commands = ['Open', 'Rename', 'Main', 'Create', 'Add', 'By', 'In']
+        self.commands = ['Open', 'Rename', 'Main', 'Create', 'Add', 'By', 'In', 'To']
+        
         # date_validator uses manageDeadlines class form date_manage this module not reusable
         # made for this app
         self.date_validator = manageDeadlines()
-        # input is going to store users input  do we need it???
+        
+        # input is going to store users input
         self.input = ''
+        
         # cur_commands is going to store recent commands from using the class
         self.cur_commands = []
 
 
     def get_command(self, search):
+        
+        # takes one argument -> string -> users input in terminal
+        # get command function looking for commands 
+        # in the users input and creates a list of commands
+        
         # cleaning cur_commands array before checking
         # because this list should contain only latest commands
         self.cur_commands = []
-        # takes one argument -> string -> users input in terminal
-        # get command function looking for commands in the users input and creates a list of commands
+        
         # why? split to separate values of the input and find commands
         for command in search.split():
             if command in self.commands:
-                # appending commands to cur_commands
+                # adding commands to cur_commands
                 self.cur_commands.append(command)
         return self
 
 
-def open_command(users_input):
-    # open -> cat => should be equal to 2 this command can not be combined with others
-    if len(users_input.split()) == 2:
-        cat_name = users_input.split()[1]
-        return redirect(url_for('show_category', name=cat_name))
-    return redirect(url_for('home_page'))
+    def open_command(self):
+
+        # Open -> cat => strict category
+        # if after split method list not greater than 1 it means only Open command
+        # was provided in input filed
+        # if greater using slice method to get category
+        
+        # using split
+        clean_input = self.input.split()
+
+        # if statement also validates if input > 1 and command in commands
+        # because Open can be written in lowercase
+        if len(clean_input) > 1 and clean_input[0] in self.commands :
+            
+            # using join with space as seperator to get string 
+            # from the sliced list because method has to return string
+            cat_name = ' '.join(clean_input[1:])
+            
+            return cat_name
+        return None
 
 
-def rename_command(users_input, db, tasks):
-    # rename needs three arguments users_input , db and Task Model
-    # Example: Rename category_name new_name or Rename old_name new_name
-    check_input = users_input.split()
-    if len(users_input.split()) == 3:
-        # if user typed everything right
-        # category to rename has index 1
-        category_rename = check_input[1]
-        # new name of the category has index 2
-        category_new_name = check_input[2]
-        # getting all tasks from db related to old category
-        # if user type category which do not exists query returns empty array
-        tasks_with_category = tasks.query.filter_by(category=category_rename).all()
+    def rename_command(self):
 
-        # if tasks_with_category = [] redirect to main page
-        if len(tasks_with_category) == 0:
+        # -> Rename old category To new category
+        # rename has two commands Rename and To
+
+        # Rename task1 To task2 -> now we need to get rid of Rename and To
+        # going to use replace method to replace commands 
+        # Rename replace with 'C' -> C for command
+        # Maybe a better option exists for this operation??!!
+        check_input = self.input.replace('To', 'C').replace('Rename', 'C')
+        
+        # and now split by ',' to get two category names
+        # after this split check_input list should have 3 elements -> '', task1, task2
+        check_input = check_input.split('C')
+       
+        # if checking input equal to 3 -> user provided legit data
+        # Less means bad request and return None
+        if len(check_input) == 3:
+            
+            # if user typed everything right
+            # category to rename has index 1
+            # and also need to do strip to get rid of bad spaces
+            category_rename = check_input[1].strip()
+            
+            # new name of the category has index 2
+            category_new_name = check_input[2].strip()
+
+            # return a list of values for renaming
+            return [category_rename, category_new_name]  
+        return None  
+
             # redirects
-            if request.args.get('category') is not None:
-                return redirect(url_for('show_category', name=category_new_name))
-            return redirect(url_for('home_page'))
-
-        # changing category name to new one for all tasks related to old category
-        for task in tasks_with_category:
-            task.category = category_new_name
-            db.session.commit()
-        # redirects
-        # check return_back() in main.py for more info
-        if request.args.get('category') is not None:
-            return redirect(url_for('show_category', name=category_new_name))
-        return redirect(url_for('home_page'))
-    return redirect(url_for('home_page'))
+            # check return_back() in main.py for more info
+        #     if request.args.get('category') is not None:
+        #         return redirect(url_for('show_category', name=category_new_name))
+        #     return redirect(url_for('home_page'))
+        # return redirect(url_for('home_page'))
 
 def create_category_add_many(users_input, db, Tasks):
 
@@ -127,3 +158,19 @@ def add_many():
 # print(by_num)
 # print(by_month)
 # print(by_month_dat)
+
+
+# db logic from rename method
+            # tasks_with_category = tasks.query.filter_by(category=category_rename).all()
+
+            # # if tasks_with_category = [] redirect to main page
+            # if len(tasks_with_category) == 0:
+            #     # redirects
+            #     if request.args.get('category') is not None:
+            #         return redirect(url_for('show_category', name=category_new_name))
+            #     return redirect(url_for('home_page'))
+
+            # # changing category name to new one for all tasks related to old category
+            # for task in tasks_with_category:
+            #     task.category = category_new_name
+            #     db.session.commit()
