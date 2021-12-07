90e0cb3c1972fd5dcf3d9a326ce1d7424bcd3fcc
lunfman <lunf@Olegs-MacBook-Air.local>
2021-12-07 10:44:14 +0200

    Created a function for calculating deadlines in the app and integrated it with main.py and jinja template

--- todoapp/main.py
+++ todoapp/main.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for
 from models import db, Tasks
 from terminal import terminalLogic
+from datetime import date, datetime
 
 app = Flask(__name__)
 
@@ -9,7 +10,6 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
 # init terminal class
 terminal_logic = terminalLogic(db)
-
 
 def get_categories():
     # get_categories function checks for all categories from db
@@ -57,6 +57,34 @@ def is_task_completed(boolean):
     
     # saving
     db.session.commit()
+
+
+def calculate_deadline(deadline):
+    
+    '''
+    this function calculates how many days left till deadline
+    and return a string with delta of days
+    if one day left it return word like tomorrow.
+    soon will add more words if needed
+    '''
+
+    # checking if deadline empty or note
+    if deadline == '':
+        return '-'
+   
+    # creating data object from deadline argument to calculate two date objs
+    # after createing date objs call method date() because date.today is a date!!!
+    deadline = datetime.strptime(deadline, '%Y-%m-%d').date()
+    delta = deadline - date.today()
+
+    delta = int(delta.days)
+
+    if delta == 1:
+        return 'tomorrow'
+    elif delta == -1:
+        return 'yesterday'
+    else:
+        return f'{delta} days'
 
 
 @app.route('/')
@@ -112,7 +140,10 @@ def show_category(name):
     # looking for tasks in this category
     current_category_todo = Tasks.query.filter_by(category=name).all()
     # return page with category name and tasks
-    return render_template('category.html', category_name=name, todo_list=current_category_todo)
+    # passing calculate_deadline function to get_deadline which will be used
+    # inside of category.html template for fetching deadlines
+    return render_template('category.html', category_name=name, 
+        todo_list=current_category_todo, get_deadline= calculate_deadline)
 
 
 if __name__ == '__main__':
---
+++ todoapp/test.py
@@ -0,0 +1,24 @@
+from datetime import date, datetime
+
+def calculate_deadline(deadline):
+    
+    '''
+    this function calculates how many days left till deadline
+    and return a string with delta of days
+    if one day left it return word like tomorrow.
+    soon will add more words if needed
+    '''
+   
+    # creating data object from deadline argument to calculate two date objs
+    # after createing date objs call method date() because date.today is a date!!!
+    deadline = datetime.strptime(deadline, '%Y-%m-%d').date()
+    delta = deadline - date.today()
+
+    delta = int(delta.days)
+
+    if delta == 1:
+        return 'tomorrow'
+    elif delta == -1:
+        return 'yesterday'
+    else:
+        return f'{delta} days'
